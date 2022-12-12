2e38e987e545760d304c05e165ab556d67b27f5b
DinoCzar <116979569+DinoCzar@users.noreply.github.com>
2022-12-12 00:38:58 -0800

    fix syntax in script.js for if functions

--- script.js
+++ script.js
@@ -2,31 +2,30 @@ function getComputerChoice() {
     return Math.floor(Math.random() * 3)
 }
 
-console.log(getComputerChoice());
-
-const playerSelection = "rock";
+const playerSelection = 0;
 const computerSelection = getComputerChoice();
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection = "rock" && computerSelection === 0) {
+    if (playerSelection === 0 && computerSelection === 0) {
     return "It's a tie! Rock ties with Rock.";
-    } else if (playerSelection = "rock" && computerSelection === 1) {
+    } else if (playerSelection === 0 && computerSelection === 1) {
     return "You lost! Paper beats Rock.";
-    } else if (playerSelection = "rock" && computerSelection === 2) {
+    } else if (playerSelection === 0 && computerSelection === 2) {
     return "You won! Rock beats Scissors.";
-    } else if (playerSelection = "paper" && computerSelection === 0) {
+    } else if (playerSelection === 1 && computerSelection === 0) {
     return "You won! Paper beats Rock.";
-    } else if (playerSelection = "paper" && computerSelection === 1) {
+    } else if (playerSelection === 1 && computerSelection === 1) {
     return "It's a tie! Paper ties with Paper.";
-    } else if (playerSelection = "paper" && computerSelection === 2) {
+    } else if (playerSelection === 1 && computerSelection === 2) {
     return "You lost! Scissors beats Paper.";
-    } else if (playerSelection = "scissors" && computerSelection === 0) {
+    } else if (playerSelection === 2 && computerSelection === 0) {
     return "You lost! Rock beats Scissors.";
-    } else if (playerSelection = "scissors" && computerSelection === 1) {
+    } else if (playerSelection === 2 && computerSelection === 1) {
     return "You won! Scissors beats Paper.";
-    } else if (playerSelection = "scissors" && computerSelection === 2) {
+    } else if (playerSelection === 2 && computerSelection === 2) {
     return "It's a tie! Scissors ties with Scissors.";
     }
 }
 
 console.log(playRound(playerSelection, computerSelection));
+
