4b7e16f1f74397c728853e214ba7e6fa7eb71c58
DinoCzar <116979569+DinoCzar@users.noreply.github.com>
2022-12-11 23:17:17 -0800

    Add text to playRound returns

--- script.js
+++ script.js
@@ -9,25 +9,24 @@ const computerSelection = getComputerChoice();
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection = "rock" && computerSelection === 0) {
-    return "It's a tie!";
+    return "It's a tie! Rock ties with Rock.";
     } else if (playerSelection = "rock" && computerSelection === 1) {
-    return "You lost!";
+    return "You lost! Paper beats Rock.";
     } else if (playerSelection = "rock" && computerSelection === 2) {
-    return "You won!";
+    return "You won! Rock beats Scissors.";
     } else if (playerSelection = "paper" && computerSelection === 0) {
-    return "You won!";
+    return "You won! Paper beats Rock.";
     } else if (playerSelection = "paper" && computerSelection === 1) {
-    return "It's a tie!";
+    return "It's a tie! Paper ties with Paper.";
     } else if (playerSelection = "paper" && computerSelection === 2) {
-    return "You lost!";
+    return "You lost! Scissors beats Paper.";
     } else if (playerSelection = "scissors" && computerSelection === 0) {
-    return "You lost!";
+    return "You lost! Rock beats Scissors.";
     } else if (playerSelection = "scissors" && computerSelection === 1) {
-    return "You won!";
+    return "You won! Scissors beats Paper.";
     } else if (playerSelection = "scissors" && computerSelection === 2) {
-    return "It's a tie!";
+    return "It's a tie! Scissors ties with Scissors.";
     }
 }
 
 console.log(playRound(playerSelection, computerSelection));
-console.log("Testing to make sure it's all good");
