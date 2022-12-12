368490bd31a3212e1f5862e39cfaab142fccb0a6
DinoCzar <116979569+DinoCzar@users.noreply.github.com>
2022-12-11 22:51:43 -0800

    add variables to script.js and rename index.html title

---
+++ script.js
@@ -0,0 +1,32 @@
+function getComputerChoice() {
+    return Math.floor(Math.random() * 3)
+}
+
+console.log(getComputerChoice());
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection = "rock" && computerSelection === 0) {
+    return "It's a tie!";
+    } else if (playerSelection = "rock" && computerSelection === 1) {
+    return "You lost!";
+    } else if (playerSelection = "rock" && computerSelection === 2) {
+    return "You won!";
+    } else if (playerSelection = "paper" && computerSelection === 0) {
+    return "You won!";
+    } else if (playerSelection = "paper" && computerSelection === 1) {
+    return "It's a tie!";
+    } else if (playerSelection = "paper" && computerSelection === 2) {
+    return "You lost!";
+    } else if (playerSelection = "scissors" && computerSelection === 0) {
+    return "You lost!";
+    } else if (playerSelection = "scissors" && computerSelection === 1) {
+    return "You won!";
+    } else if (playerSelection = "scissors" && computerSelection === 2) {
+    return "It's a tie!";
+    }
+}
+
+console.log(playRound(playerSelection, computerSelection));
