db2c2dd90b9bdab18d1e0a48ab916e66841a50a7
DinoCzar <116979569+DinoCzar@users.noreply.github.com>
2023-01-10 01:25:01 -0800

    Add buttons and event listeners for buttons

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ var playerScore = 0;
 
 var computerScore = 0;
 
+/*
+
 function playRound() {
     var playerSelection = prompt("Enter Rock, Paper, or Scissors:"); 
     var computerSelection = myArray[Math.floor(Math.random() * myArray.length)];
@@ -22,8 +24,11 @@ function playRound() {
     }
 }
 
+
+
+
 function game() {
-    for (var i = 1; i < 7; i++) {
+    for (let i = 1; i < 7; i++) {
         if (i<6) {
             console.log(playRound());
         } else {
@@ -39,4 +44,36 @@ function game() {
 }
 
 game();
+
+*/
+
+const container = document.querySelector('#container');
+const content = document.createElement('div');
+container.appendChild(content);
+
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        var computerSelection = myArray[Math.floor(Math.random() * myArray.length)];
+        var playerSelection = button.id;
+        if (playerScore === 5) {
+            content.textContent = "Player Wins!"
+        } else if (computerScore === 5) {
+            content.textContent = "Computer Wins!"
+        } else if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
+            content.textContent = (playerSelection + " vs " + computerSelection + ". Player Score: " + playerScore + " / Computer Score: " + computerScore);
+        } else if ((playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "paper") || 
+            (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "scissors") || 
+            (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "rock")) {
+            content.textContent = (playerSelection + " vs " + computerSelection + ". Player Score: " + playerScore + " / Computer Score: " + ++computerScore);
+        } else if ((playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "scissors") || 
+            (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "rock") || 
+            (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "paper")) {
+            content.textContent = (playerSelection + " vs " + computerSelection + ". Player Score: " + ++playerScore + " / Computer Score: " + computerScore);
+        } else {
+            content.textContent = ("Not a valid entry, please enter Rock, Paper, or Scissors");
+        }    
+    });
+});
 
